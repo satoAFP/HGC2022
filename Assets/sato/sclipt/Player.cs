fafead9e98dc4e5fecd8954816396612ea3ae33d
satoAFP <72423443+satoAFP@users.noreply.github.com>
2021-10-15 12:35:01 +0900

    ボタンでアクション選択、アクションブロックに乗ると選択順にアクション開始

--- Assets/sato/sclipt/Player.cs
+++ Assets/sato/sclipt/Player.cs
@@ -20,11 +20,34 @@ public class Player : MonoBehaviour
     private bool Jump_Flag = true;  //ジャンプしているかのフラグ
     private float inputX = 0;       //X軸の移動ベクトル
     private float inputZ = 1;       //Z軸の移動ベクトル
+    private int Select_order = 0;   //ボタンを押された順番を記憶
+    private int Action_count = 0;   //アクションをした回数をカウント
+    private bool Action_check = false;//アクションを一回しか使えないよう管理
+
+
+    //構造体-------------------------------------------------------------------
+    //ボタン使用時周り
+    private struct Buttan
+    {
+        public bool push;      //ボタンが押されたかの判定
+        public int push_num;   //押された時の順番記憶
+        //初期化用関数
+        public Buttan(bool a, int b) {
+            push = a;
+            push_num = b;
+        }
+    }
+    //構造体の初期化
+    Buttan jump = new Buttan(false, 0);
+    Buttan squat = new Buttan(false, 0);
+    Buttan stick = new Buttan(false, 0);
+    Buttan stop = new Buttan(false, 0);
 
 
     // Start is called before the first frame update
     void Start() {
         push = new Vector3(0.0f, push_power, 0.0f);
+
     }
 
     // Update is called once per frame
@@ -33,21 +56,26 @@ public class Player : MonoBehaviour
         //移動処理
         MOVE(inputX, inputZ);
 
-        //ジャンプ操作
-        if (transform.position.y <= 1.0f || Max_Jmup - 1 != Jump_Count) {
-            if (Input.GetKey(KeyCode.Space)) {
-                if (Jump_Flag == true) {
-                    this.GetComponent<Rigidbody>().AddForce(push, ForceMode.Impulse);
-                    Jump_Count++;
-                    Jump_Flag = false;
+        //選択した順番とアクションブロックに乗った順番が連動する
+        if (jump.push_num == Action_count && Action_check == true) {
+            //ジャンプ操作
+            if (transform.position.y <= 1.0f || Max_Jmup - 1 != Jump_Count) {
+                if (jump.push == true) {
+                    if (Jump_Flag == true) {
+                        this.GetComponent<Rigidbody>().AddForce(push, ForceMode.Impulse);
+                        Jump_Count++;
+                        Jump_Flag = false;
+                    }
+                }
+                else {
+                    Jump_Flag = true;
                 }
             }
-            else {
-                Jump_Flag = true;
+            if (transform.position.y <= 1.0f) {
+                Jump_Count = 0;
             }
-        }
-        if(transform.position.y<=1.0f) {
-            Jump_Count = 0;
+            jump.push = false;
+            Action_check = false;
         }
     }
 
@@ -79,7 +107,11 @@ public class Player : MonoBehaviour
                     inputX = 0;
                     break;
             }
+        }
 
+        if (collision.gameObject.tag == "Action") {
+            Action_count++;
+            Action_check = true;
         }
 
     }
@@ -96,4 +128,31 @@ public class Player : MonoBehaviour
         float moveZ = z * moveSpeed * Time.deltaTime;
         transform.Translate(moveX, 0.0f, moveZ);
     }
+
+    //-----------------------------------------------------------------------------------
+    //ボタンでの操作選択
+    public void Push_jump() {
+        jump.push = true;
+        Select_order++;
+        jump.push_num = Select_order;
+    }
+
+    public void Push_squat() {
+        squat.push = true;
+        Select_order++;
+        squat.push_num = Select_order;
+    }
+
+    public void Push_stick() {
+        stick.push = true;
+        Select_order++;
+        stick.push_num = Select_order;
+    }
+
+    public void Push_stop() {
+        stop.push = true;
+        Select_order++;
+        stop.push_num = Select_order;
+    }
+
 }
