760a6be39e3890c5cf45905a9718ef37856d10c1
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2021-10-22 12:10:53 +0900

    アクションボタンScript更新、tatumi内のActionButton_CSを入れてください。 主に、リザルトまでのオブジェクト残存、複数枚複製です。
    α-test内で一通りのシーン繋ぎ完了。

---
+++ Assets/tatumi/Script/ActionButton_SC.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+
+public class ActionButton_SC : MonoBehaviour
+{
+    [Header("このシーン以外は表示しない")]
+    [Header("特定しか存在しない-------------------------")]
+    public string SceneName;
+
+    [Header("必ず自身を指定")]
+    public GameObject Object;
+
+   
+    [Header("子の要素数")]
+    [Header("複数枚表示---------------------------------")]
+    public int Child_num;
+
+    [Header("複製数指定")]
+    public int[] Duplicate;
+
+    [Header("複製数指定")]
+    public GameObject[] childGameObjects;
+   
+
+    private string NowStage;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        NowStage = SceneManager.GetActiveScene().name;
+
+        for (int i = 0; i != Child_num; i++)
+        {
+            for (int k = 0; k != Duplicate[i]; k++)
+            {
+               GameObject newObj = Instantiate(childGameObjects[i], this.transform, false);
+            }
+        }
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == SceneName|| SceneManager.GetActiveScene().name==NowStage)
+        {
+            ;
+        }
+        else
+            Destroy(Object, .01f);
+    }
+}
