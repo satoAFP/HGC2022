efc5d4187387b7a1b9603ea0926e421045812dd5
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2021-11-11 15:42:22 +0900

    マルチ行動更新

--- Assets/tatumi/Script/ButtonChoice.cs
+++ Assets/tatumi/Script/ButtonChoice.cs
@@ -32,21 +32,7 @@ public class ButtonChoice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pos = this.gameObject.transform.position;
-
-        if (pos.x == 800.0f)
-        {
-            Debug.Log("tag1 get");//ok
-            this.tag = "Multi_action1";
-        }
-        else if (pos.x == 1000.0f)
-            this.tag = "Multi_action2";
-        else if (pos.x == 1200.0f)
-            this.tag = "Multi_action3";
-        else if (pos.x == 1400.0f)
-            this.tag = "Multi_action4";
-        else
-            this.tag = "Untagged";
+       
 
        
     }
@@ -84,6 +70,22 @@ public class ButtonChoice : MonoBehaviour
                 this.gameObject.transform.position = new Vector3(pos.x+200.0f, pos.y, pos.z);
             else
                 this.gameObject.transform.position = new Vector3(first_x, pos.y, pos.z);
+
+            pos = this.gameObject.transform.position;
+
+            if (pos.x == 800.0f && this.tag != "Multi_action1")
+            {
+                Debug.Log("tag1 get");//ok
+                this.tag = "Multi_action1";
+            }
+            else if (pos.x == 1000.0f)
+                this.tag = "Multi_action2";
+            else if (pos.x == 1200.0f)
+                this.tag = "Multi_action3";
+            else if (pos.x == 1400.0f)
+                this.tag = "Multi_action4";
+            else
+                this.tag = "Untagged";
         }
         //ê^ÇÒíÜÉNÉä
         else if(Input.GetMouseButtonDown(2))
--- Assets/tatumi/Script/Multi_Action_move.cs
+++ Assets/tatumi/Script/Multi_Action_move.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Highjump : MonoBehaviour
+public class Multi_Action_move : MonoBehaviour
 {
+    [Header("合体番号")]
+    public int action_num;
+
+    public GameObject player; //参照元OBJそのものが入る変数
+
+   // Player script; //参照元Scriptが入る変数
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player"); //ActionButtonをオブジェクトの名前から取得して変数に格納する
+       // script = player.GetComponent<Player>(); //OBJの中にあるScriptを取得して変数に格納する
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    public void PushButton()
+    {
+        if (action_num == 0)
+        {
+            player.GetComponent<Player>().Push_highjump();
+        }
     }
 }
