6e4976d3e72ec2410b29bc94a58824535eb3848a
satoAFP <72423443+satoAFP@users.noreply.github.com>
2021-12-16 16:02:41 +0900

    選択したカードを表示時の画像変更

---
+++ Assets/sato/sclipt/Select_Card_Manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Select_Card_Manager : MonoBehaviour
+{
+    [Header("イメージ表示用オブジェクト")]
+    public GameObject[] Select;
+
+    [Header("変更画像")]
+    public Sprite[] Card_img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
