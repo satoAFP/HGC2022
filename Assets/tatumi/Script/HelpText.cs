2ef568fc15fa317e0f368a324ec188691cb3ccab
DEV2-35\GA3 <h1202019035@hiratagakuen.onmicrosoft.com>
2022-01-18 10:46:18 +0900

    チュートリアル追加

--- Assets/tatumi/Script/HelpText.cs
+++ Assets/tatumi/Script/HelpText.cs
@@ -9,15 +9,17 @@ public class HelpText : MonoBehaviour
 
     public Text text;
 
-    public GameObject yazirusi;
+    public GameObject[] yazirusi = new GameObject[2];
     public GameObject oya;
     public GameObject PL;
     public GameObject Card;
     public GameObject CardChoice;
     public GameObject BackButton;
+    public GameObject ActionButton;
 
     Player SC_player;
     DeletAction SC_Back;
+    ActionButton_SC SC_action; //éQè∆å≥ScriptÇ™ì¸ÇÈïœêî
 
     public int nowtext;
 
@@ -33,6 +35,7 @@ public class HelpText : MonoBehaviour
     {
         SC_player = PL.GetComponent<Player>();
         SC_Back = BackButton.GetComponent<DeletAction>();
+        SC_action = ActionButton.GetComponent<ActionButton_SC>();
     }
 
     // Update is called once per frame
@@ -57,22 +60,23 @@ public class HelpText : MonoBehaviour
         if (nowtext < 0)
             nowtext = 0;
 
-        if(nowtext==2)
-        {
-            if(Input.GetKey(KeyCode.Return))
-            {
-                if (Helpmode == 0)
-                {
-                    oya.SetActive(false);
-                    SC_player.Movestop = false;
-                }
-            }
-        }
+       
 
         if(Helpmode==0)
         {
             if (nowtext > Endchar[0])
                 nowtext = Endchar[0];
+
+            if (nowtext == 2)
+            {
+                if (Input.GetKey(KeyCode.Return))
+                {
+                   
+                    oya.SetActive(false);
+                    SC_player.Movestop = false;
+                    
+                }
+            }
         }
         else if(Helpmode==1)
         {
@@ -83,8 +87,8 @@ public class HelpText : MonoBehaviour
                 Card.SetActive(true);
                 if (nowtext == 3)
                 {
-                    yazirusi.gameObject.transform.position = new Vector3(63.95f, 299.6f, 0.0f);
-                    yazirusi.SetActive(true);
+                   
+                    yazirusi[0].SetActive(true);
                 }
             }
            
@@ -97,7 +101,7 @@ public class HelpText : MonoBehaviour
                 SC_player.Movestop = false;
                
                 Card.SetActive(false);
-                yazirusi.SetActive(false);
+                yazirusi[0].SetActive(false);
 
                 SC_Back.now--;
             }
@@ -111,8 +115,8 @@ public class HelpText : MonoBehaviour
                 if (nowtext == 5)
                 {
                     CardChoice.SetActive(true);
-                    yazirusi.gameObject.transform.position = new Vector3(203.0f, 299.6f, 0.0f);
-                    yazirusi.SetActive(true);
+                  
+                    yazirusi[1].SetActive(true);
                 }
             }
 
@@ -125,8 +129,74 @@ public class HelpText : MonoBehaviour
                 SC_player.Movestop = false;
 
                 CardChoice.SetActive(false);
-                yazirusi.SetActive(false);
+                yazirusi[1].SetActive(false);
 
+            }
+        }
+        else if (Helpmode == 3)
+        {
+            if (nowtext < Endchar[2])
+                nowtext = Endchar[2];
+            else if (yazirusi[1].activeSelf == true)
+            {
+                if (nowtext < Endchar[2]+1)
+                    nowtext = Endchar[2] + 1;
+            }
+
+            if (nowtext >= 7 && nowtext <=10)
+            {
+               Card.SetActive(true);
+
+               for (int i = 0; i != 2; i++)
+               {
+                  yazirusi[i].SetActive(true);
+               }
+
+            }
+            
+
+            if (nowtext > Endchar[Helpmode] - 1)
+                nowtext = Endchar[Helpmode] - 1;
+
+            if (SC_Back.multi_objs[0].name.Contains("multi_highjump(Clone)")==true)
+            {
+               
+                yazirusi[0].SetActive(false);
+                
+
+                yazirusi[1].gameObject.transform.position = new Vector3(23.0f, 299.6f, -102.0f);
+            }
+
+            if (SC_action.multi_des[0].name.Contains("multi_highjump(Clone)") == true)
+            {
+                oya.SetActive(false);
+                SC_player.Movestop = false;
+
+                Card.SetActive(false);
+                for (int i = 0; i != 2; i++)
+                {
+                    yazirusi[i].SetActive(false);
+                }
+
+            }
+        }
+        else if (Helpmode == 4)
+        {
+            if (nowtext > Endchar[3])
+                nowtext = Endchar[3];
+
+            if (nowtext > Endchar[Helpmode] - 1)
+                nowtext = Endchar[Helpmode] - 1;
+
+            if (nowtext == Endchar[Helpmode] - 1)
+            {
+                if (Input.GetKey(KeyCode.Return))
+                {
+
+                    oya.SetActive(false);
+                    SC_player.Movestop = false;
+
+                }
             }
         }
 
