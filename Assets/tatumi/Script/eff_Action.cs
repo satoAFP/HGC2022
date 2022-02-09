74f020fb8c35459e6a866448fb9d2ba463a522a7
晃聖 辰巳 <h1202019035@hiratagakuen.onmicrosoft.com>
2022-02-09 13:04:08 +0900

    アクションエフェクトの関数追記

--- Assets/tatumi/Script/eff_Action.cs
+++ Assets/tatumi/Script/eff_Action.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class eff_Action : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    public void Copied()
+    {
+        
+            GameObject copied = Object.Instantiate(this.gameObject) as GameObject;
+
+            eff_Action script = copied.gameObject.GetComponent<eff_Action>();
+
+            Destroy(script);
+            Destroy(copied,1.0f);
         
     }
 }
